5d19c17148c95317dbf8216e9d49fece88f5a7f5
aserafim <correia.alefe@gmail.com>
2024-05-31 10:32:00 -0300

    Finalizado o post do formulário

--- public/script.js
+++ public/script.js
@@ -14,17 +14,39 @@ async function carregarAnimais(){
 function manipularFormulario(){
     const form_animal = document.getElementById('form-animal')
     const input_nome = document.getElementById('nome')
+    const input_idade = document.getElementById('idade')
+    const input_sexo = document.getElementById('sexo')
+    const input_cor = document.getElementById('cor')
 
-    const nome_animal = input_nome.value
-        alert(`submit chamado...${nome_animal}`)
+    form_animal.onsubmit = () =>{
+        event.preventDefault()
+        const nome_animal = input_nome.value
+        const idade_animal = input_idade.value
+        const sexo_animal = input_sexo.value
+        const cor_animal = input_cor.value
+        
+        //alert(`Valores inputados ${nome_animal}, ${idade_animal}, ${sexo_animal}, ${cor_animal}`)
+
+
+
+        axios.post('http://localhost:8080/animais',{
+            id : null,
+            nome: nome_animal,
+            idade: Number(idade_animal),
+            sexo: sexo_animal,
+            cor: cor_animal
+        })
+
     }
-    
+
+
+}
+
+//main
 function app(){
-    console.log("App iniciada.")
     carregarAnimais()
     manipularFormulario()
 }
 
+//executa a main
 app()
-
-carregarAnimais()
